24d857f278b8c6788be36ce78fd8b319787ca153
ARipeAppleByYoursTruly <83992779+ARipeAppleByYoursTruly@users.noreply.github.com>
2022-04-24 16:27:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+// ===== include HTMLs =====
+w3.includeHTML(main);
+
+
+
+function main() {
+    // ===== Events =====
+    // ----- Toggle top nav list in mobile -----
+    // Menu button
+    document.querySelector(".top-nav .menu").onclick = function() {
+        // List of <li>
+        document.querySelectorAll(".top-nav ul li").forEach(function(current_li) {
+            // Ignore logo
+            if (current_li.classList.contains("to-home")) {
+                return;
+            }
+            else {
+                var current_div = current_li.getElementsByTagName("div")[0];
+
+                if (window.getComputedStyle(current_div).display == "none") {
+                    current_div.style.display = "block";
+                }
+                else {
+                    current_div.style.display = "none";
+                }
+            }
+        });
+    }
+}
